10a3693c04aa081034230d561894198ffa895099
mutoe <mutoe@foxmail.com>
2022-02-13 02:51:35 +0800

    feat: format currency using intl

---
+++ lib/constants/formatter.dart
@@ -0,0 +1,12 @@
+import 'package:intl/intl.dart';
+
+final currencyYuanFormatter = NumberFormat.currency(
+  locale: 'zh_CN',
+  symbol: '¥',
+  decimalDigits: 2,
+);
+final currencyDollarFormatter = NumberFormat.currency(
+  locale: 'en_US',
+  symbol: '\$',
+  decimalDigits: 2,
+);
--- lib/wallet/screen/wallet_home_screen.dart
+++ lib/wallet/screen/wallet_home_screen.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:handnote/constants/formatter.dart';
+
+const double bannerHeight = 180;
 
 class WalletHomeScreen extends HookWidget {
   const WalletHomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    const double bannerHeight = 180;
-    var titleOpacity = useState<double>(0);
-    var scrollController = useScrollController();
+    double amount = 1591.0;
+    double income = 1;
+
+    final titleOpacity = useState<double>(0);
+    final scrollController = useScrollController();
     scrollController.addListener(() {
       titleOpacity.value = scrollController.offset > bannerHeight * 0.6 ? 1 : 0;
     });
@@ -35,16 +40,18 @@ class WalletHomeScreen extends HookWidget {
             ),
             flexibleSpace: FlexibleSpaceBar(
               background: Padding(
-                padding: const EdgeInsets.all(16.0),
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   crossAxisAlignment: CrossAxisAlignment.start,
-                  children: const [
-                    Text("本月支出（元）", style: TextStyle(fontSize: 14, color: Colors.white70)),
-                    SizedBox(height: 8),
-                    Text("¥1,590.00", style: TextStyle(fontSize: 32, color: Colors.white)),
-                    SizedBox(height: 8),
-                    Text("本月收入 暂无收入 设置预算", style: TextStyle(fontSize: 14, color: Colors.white70)),
+                  children: [
+                    const Text("本月支出（元）", style: TextStyle(fontSize: 14, color: Colors.white70)),
+                    const SizedBox(height: 8),
+                    Text(currencyYuanFormatter.format(amount),
+                        style: const TextStyle(fontSize: 32, color: Colors.white)),
+                    const SizedBox(height: 8),
+                    Text("本月收入 ${income > 0 ? currencyYuanFormatter.format(income) : '暂无收入'} 设置预算",
+                        style: const TextStyle(fontSize: 14, color: Colors.white70)),
                   ],
                 ),
               ),
@@ -53,62 +60,62 @@ class WalletHomeScreen extends HookWidget {
           SliverList(
             delegate: SliverChildListDelegate([
               const SizedBox(height: 20),
-              Text(
+              const Text(
                 'Welcome to Handnote!',
-                style: TextStyle(fontSize: 20),
+                style: const TextStyle(fontSize: 20),
               ),
               const SizedBox(height: 20),
-              Text(
+              const Text(
                 'Handnote is a simple, secure, and easy-to-use wallet for your digital assets.',
                 style: TextStyle(fontSize: 16),
               ),
               const SizedBox(height: 20),
-              Text(
+              const Text(
                 'You can use Handnote to store and manage your digital assets, and send them to other people.',
                 style: TextStyle(fontSize: 16),
               ),
               const SizedBox(height: 20),
-              Text(
+              const Text(
                 'Handnote is a decentralized, open-source, and open-source wallet for your digital assets.',
                 style: TextStyle(fontSize: 16),
               ),
               const SizedBox(height: 20),
-              Text(
+              const Text(
                 'You can use Handnote to store and manage your digital assets, and send them to other people.',
                 style: TextStyle(fontSize: 16),
               ),
               const SizedBox(height: 20),
-              Text(
+              const Text(
+                'Handnote is a decentralized, open-source, and open-source wallet for your digital assets.',
+                style: const TextStyle(fontSize: 16),
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'You can use Handnote to store and manage your digital assets, and send them to other people.',
+                style: TextStyle(fontSize: 16),
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'Handnote is a decentralized, open-source, and open-source wallet for your digital assets.',
+                style: const TextStyle(fontSize: 16),
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'You can use Handnote to store and manage your digital assets, and send them to other people.',
+                style: const TextStyle(fontSize: 16),
+              ),
+              const SizedBox(height: 20),
+              const Text(
                 'Handnote is a decentralized, open-source, and open-source wallet for your digital assets.',
                 style: TextStyle(fontSize: 16),
               ),
               const SizedBox(height: 20),
-              Text(
+              const Text(
                 'You can use Handnote to store and manage your digital assets, and send them to other people.',
                 style: TextStyle(fontSize: 16),
               ),
               const SizedBox(height: 20),
-              Text(
-                'Handnote is a decentralized, open-source, and open-source wallet for your digital assets.',
-                style: TextStyle(fontSize: 16),
-              ),
-              const SizedBox(height: 20),
-              Text(
-                'You can use Handnote to store and manage your digital assets, and send them to other people.',
-                style: TextStyle(fontSize: 16),
-              ),
-              const SizedBox(height: 20),
-              Text(
-                'Handnote is a decentralized, open-source, and open-source wallet for your digital assets.',
-                style: TextStyle(fontSize: 16),
-              ),
-              const SizedBox(height: 20),
-              Text(
-                'You can use Handnote to store and manage your digital assets, and send them to other people.',
-                style: TextStyle(fontSize: 16),
-              ),
-              const SizedBox(height: 20),
-              Text(
+              const Text(
                 'Handnote is a decentralized, open-source, and open-source wallet for your digital assets.',
                 style: TextStyle(fontSize: 16),
               ),
