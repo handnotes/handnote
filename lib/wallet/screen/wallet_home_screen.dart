8580e2e509981ab3f6964a555256aabaf6d7fbd4
mutoe <mutoe@foxmail.com>
2022-02-13 03:11:34 +0800

    feat: optimize setup budget style

--- lib/wallet/screen/wallet_home_screen.dart
+++ lib/wallet/screen/wallet_home_screen.dart
@@ -9,9 +9,6 @@ class WalletHomeScreen extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
-    double amount = 1591.0;
-    double income = 1;
-
     final titleOpacity = useState<double>(0);
     final scrollController = useScrollController();
     scrollController.addListener(() {
@@ -22,47 +19,13 @@ class WalletHomeScreen extends HookWidget {
       body: CustomScrollView(
         controller: scrollController,
         slivers: [
-          SliverAppBar(
-            pinned: true,
-            expandedHeight: bannerHeight,
-            stretch: true,
-            titleSpacing: 0,
-            title: AnimatedOpacity(
-              duration: const Duration(milliseconds: 300),
-              opacity: titleOpacity.value,
-              child: AppBar(
-                title: const Text('Handnote'),
-                leading: IconButton(
-                  icon: const Icon(Icons.book),
-                  onPressed: () {},
-                ),
-              ),
-            ),
-            flexibleSpace: FlexibleSpaceBar(
-              background: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text("本月支出（元）", style: TextStyle(fontSize: 14, color: Colors.white70)),
-                    const SizedBox(height: 8),
-                    Text(currencyYuanFormatter.format(amount),
-                        style: const TextStyle(fontSize: 32, color: Colors.white)),
-                    const SizedBox(height: 8),
-                    Text("本月收入 ${income > 0 ? currencyYuanFormatter.format(income) : '暂无收入'} 设置预算",
-                        style: const TextStyle(fontSize: 14, color: Colors.white70)),
-                  ],
-                ),
-              ),
-            ),
-          ),
+          _appBar(titleOpacity),
           SliverList(
             delegate: SliverChildListDelegate([
               const SizedBox(height: 20),
               const Text(
                 'Welcome to Handnote!',
-                style: const TextStyle(fontSize: 20),
+                style: TextStyle(fontSize: 20),
               ),
               const SizedBox(height: 20),
               const Text(
@@ -87,7 +50,7 @@ class WalletHomeScreen extends HookWidget {
               const SizedBox(height: 20),
               const Text(
                 'Handnote is a decentralized, open-source, and open-source wallet for your digital assets.',
-                style: const TextStyle(fontSize: 16),
+                style: TextStyle(fontSize: 16),
               ),
               const SizedBox(height: 20),
               const Text(
@@ -97,12 +60,12 @@ class WalletHomeScreen extends HookWidget {
               const SizedBox(height: 20),
               const Text(
                 'Handnote is a decentralized, open-source, and open-source wallet for your digital assets.',
-                style: const TextStyle(fontSize: 16),
+                style: TextStyle(fontSize: 16),
               ),
               const SizedBox(height: 20),
               const Text(
                 'You can use Handnote to store and manage your digital assets, and send them to other people.',
-                style: const TextStyle(fontSize: 16),
+                style: TextStyle(fontSize: 16),
               ),
               const SizedBox(height: 20),
               const Text(
@@ -121,6 +84,68 @@ class WalletHomeScreen extends HookWidget {
               ),
             ]),
           )
+        ],
+      ),
+    );
+  }
+
+  Widget _appBar(ValueNotifier<double> titleOpacity) {
+    double amount = 1591.0;
+    double income = 1;
+
+    return SliverAppBar(
+      pinned: true,
+      expandedHeight: bannerHeight,
+      stretch: true,
+      titleSpacing: 0,
+      title: AnimatedOpacity(
+        duration: const Duration(milliseconds: 300),
+        opacity: titleOpacity.value,
+        child: AppBar(
+          title: const Text('Handnote'),
+          leading: IconButton(
+            icon: const Icon(Icons.book),
+            onPressed: () {},
+          ),
+        ),
+      ),
+      flexibleSpace: FlexibleSpaceBar(
+        background: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text("本月支出（元）", style: TextStyle(fontSize: 14, color: Colors.white70)),
+              const SizedBox(height: 8),
+              Text(currencyYuanFormatter.format(amount), style: const TextStyle(fontSize: 32, color: Colors.white)),
+              const SizedBox(height: 12),
+              Row(
+                children: [
+                  Expanded(
+                    child: Text("本月收入\t${income > 0 ? currencyYuanFormatter.format(income) : '暂无收入'}",
+                        style: const TextStyle(fontSize: 14, color: Colors.white70)),
+                  ),
+                  Expanded(child: _setupBudgetWidget()),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _setupBudgetWidget() {
+    return GestureDetector(
+      onTap: () {
+        // TODO: setup budget page
+      },
+      child: Row(
+        children: const [
+          Text("设置预算", style: TextStyle(fontSize: 14, color: Colors.white70)),
+          SizedBox(width: 8),
+          Icon(Icons.admin_panel_settings_outlined, color: Colors.white70, size: 18),
         ],
       ),
     );
