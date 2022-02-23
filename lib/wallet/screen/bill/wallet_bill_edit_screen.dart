277525b464de59d38f87513fd288e3bbc369b2ce
mutoe <mutoe@foxmail.com>
2022-02-23 20:50:01 +0800

    feat: implement date pick and description edit in bill page
    Took 41 minutes
    
    Took 6 minutes
    
    
    Took 2 minutes

--- lib/wallet/screen/bill/wallet_bill_edit_screen.dart
+++ lib/wallet/screen/bill/wallet_bill_edit_screen.dart
@@ -13,10 +13,22 @@ class WalletBillEditScreen extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, ref) {
+    final theme = Theme.of(context);
     final walletAssets = ref.watch(walletAssetProvider);
+
     final billType = useState(WalletBillType.outcome);
     final asset = useState<WalletAsset?>(null);
     final amount = useState(0.0);
+    final time = useState<DateTime>(DateTime.now());
+    final description = useState<String?>(null);
+
+    final today = DateTime.now();
+    final timeDiff = today.difference(time.value);
+    final String timeLabel = timeDiff.inDays == 0
+        ? '今天'
+        : timeDiff.inDays > 365
+            ? '${time.value.year}年${time.value.month}月${time.value.day}日'
+            : '${time.value.month}月${time.value.day}日';
 
     return PageContainer(
       child: Scaffold(
@@ -30,18 +42,53 @@ class WalletBillEditScreen extends HookConsumerWidget {
             onSelected: (index) => billType.value = WalletBillType.values[index],
           ),
         ),
-        body: Column(
-          children: [
-            WalletBillEditAmount(
-              billType: billType.value,
-              asset: asset.value,
-              assets: walletAssets,
-              onSelected: (newAsset) => asset.value = newAsset,
-              amount: amount.value,
-              onAmountChanged: (newValue) => amount.value = newValue,
+        body: Column(children: [
+          WalletBillEditAmount(
+            billType: billType.value,
+            asset: asset.value,
+            assets: walletAssets,
+            onSelected: (newAsset) => asset.value = newAsset,
+            amount: amount.value,
+            onAmountChanged: (newValue) => amount.value = newValue,
+          ),
+          Container(height: 16, color: theme.backgroundColor),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: IntrinsicHeight(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  ConstrainedBox(
+                    constraints: const BoxConstraints(minWidth: 100, maxHeight: double.infinity),
+                    child: TextButton.icon(
+                      icon: const Icon(Icons.calendar_today_sharp),
+                      label: Text(timeLabel),
+                      onPressed: () async {
+                        time.value = await showDatePicker(
+                              context: context,
+                              initialDate: DateTime.now(),
+                              firstDate: DateTime(2000),
+                              lastDate: today,
+                            ) ??
+                            DateTime.now();
+                      },
+                    ),
+                  ),
+                  const VerticalDivider(),
+                  Expanded(
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        hintText: '备注',
+                      ),
+                      initialValue: description.value,
+                      onChanged: (newValue) => description.value = newValue,
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ],
-        ),
+          ),
+        ]),
       ),
     );
   }
