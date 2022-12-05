e5681985a78b479ecf11ca6018573094145780c2
mutoe <imutoe@gmail.com>
2022-12-05 19:43:49 +0800

    improve: optimize init database processing

---
+++ lib/config.dart
@@ -0,0 +1,1 @@
+const currentDatabaseVersion = 5;
--- lib/database/db.dart
+++ lib/database/db.dart
@@ -1,6 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:handnote/config.dart';
 import 'package:handnote/main.dart';
 import 'package:logging/logging.dart';
 import 'package:path/path.dart' as path;
@@ -15,18 +18,37 @@ class DB with ChangeNotifier {
 
   static Database? _database;
 
+  bool _isProcessing = false;
+
   Future<Database> get instance async {
     if (_database != null) return _database!;
+    if (_isProcessing) {
+      logger.finest('Waiting for instance init...');
+      await Future.doWhile(() async {
+        await Future.delayed(const Duration(milliseconds: 10));
+        if (_database != null) return false;
+        return true;
+      });
+      logger.finest('Found instance...');
+      return _database!;
+    }
+    logger.finest('Init instance...');
+    _isProcessing = true;
     _database = await _initDatabase();
+    _isProcessing = false;
+    logger.finest('Init instance success...');
     return _database!;
   }
 
   static Future<void> cleanDatabase({BuildContext? context}) async {
     logger.info('Cleaning database...');
-    await deleteDatabase(await _getDatabaseFileName());
+    var dbFilename = await _getDatabaseFileName();
+    await _database?.close();
+    _database = null;
+    await deleteDatabase(dbFilename);
+
     if (context != null) {
-      await _database?.close();
-      _database = null;
+      logger.info('Restarting application...');
       HandnoteApp.restartApp(context);
     }
   }
@@ -53,21 +75,36 @@ class DB with ChangeNotifier {
 
     return openDatabase(
       dbPath,
-      version: 4,
+      version: currentDatabaseVersion,
+      onOpen: (db) async {
+        logger.info('Database opened. Current database version is v${await db.getVersion()}');
+      },
       onCreate: (Database db, int version) async {
         logger.info('First time run the app. Start running the migration script to v$version...');
-        for (var i = 1; i <= version; i++) {
-          await _executeSql(db, i);
-        }
-        logger.info('Setup database succeed. Version: $version');
+        return _upgradeDatabase(db, 1, version);
       },
       onUpgrade: (Database db, int oldVersion, int newVersion) async {
         logger.info('Database need to update. Running the migration script v$oldVersion -> v$newVersion...');
-        for (var i = oldVersion + 1; i <= newVersion; i++) {
-          await _executeSql(db, oldVersion);
-        }
+        return _upgradeDatabase(db, oldVersion, newVersion);
       },
     );
+  }
+
+  Future<void> _upgradeDatabase(Database db, int from, int to) async {
+    var i = from;
+    try {
+      for (; i <= to; i++) {
+        await _executeSql(db, i);
+      }
+      if (from == 1) {
+        logger.info('Setup database succeed. Current version: $to');
+      } else {
+        logger.info('Upgrade database succeed. Current version: $to (old version: $from)');
+      }
+    } catch (e) {
+      logger.severe('Database init failed when run script v$i}');
+      rethrow;
+    }
   }
 
   Future<void> _executeSql(Database db, int version) async {
@@ -75,9 +112,9 @@ class DB with ChangeNotifier {
     var scripts = (await _loadSqlFile(version)).split(';');
     for (var script in scripts) {
       final sql = script
-          .trim()
           .replaceAll(RegExp(r'--.*$', multiLine: true), '') // remove comments
-          .replaceAll(RegExp(r'\s+'), ' '); // squash whitespace
+          .replaceAll(RegExp(r'\s+'), ' ') // squash whitespace
+          .trim();
       if (sql.isEmpty) continue;
 
       logger.finer('Preparing(v$version): $sql');
